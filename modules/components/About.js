88a54a5f0da8d4d24c7465f3691979651a07fbd2
bondparkerbond <parkerbond@gmail.com>
2016-05-23 19:38:22 -0600

    use Tweet component in about to prove it is reusable

--- modules/components/About.js
+++ modules/components/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Tweet from './Tweet'
 // If you don't need lifecycle methods, it's better to just write react as a function:
 // pass in = ( props, here ) => // This is best for presentation components
 // it is a pure function and doesn't need state...
 const About = () => (
   <div>
     <h1>This is my twitter app</h1>
+    <Tweet id={4} author='Hodor' tweet='Hodor!' />
   </div>
 )
 
